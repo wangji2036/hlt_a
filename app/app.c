f506eaadf1bc5066c8d7db19884acc48ad30766b
Liu Qien <qien.liu@nuvoltatech.com>
2026-04-27 14:02:57 +0800

    打印

--- app/app.c
+++ app/app.c
@@ -112,7 +112,7 @@ void apl_task_event_handler(uint32_t event)
 			gd->sys_infos.ntc_temp_wpc = fml_ntc_temp_get_wpc();
 
 //			gd->sys_infos.die_temp = fml_die_temp_get();
-			printk("\r\n ntc_typec=%d,ntc_wpc=%d,bat_temp=%d",gd->sys_infos.ntc_temp_typec,gd->sys_infos.ntc_temp_wpc,ntc_to_temp(g_buckboost.adc_tbat1));
+			printk("\r\n ntc_typec=%d,ntc_wpc=%d,bat_temp=%d,bat_res=%d",gd->sys_infos.ntc_temp_typec,gd->sys_infos.ntc_temp_wpc,ntc_to_temp(g_buckboost.adc_tbat1),g_buckboost.adc_tbat1);
 //			fml_tntc_otp_check(gd->sys_infos.ntc_temp);
 			wpc_power_handle(gd->sys_infos.ntc_temp_wpc, ntc_to_temp(g_buckboost.adc_tbat1));
 		//	fml_tntc_utp_check(gd->sys_infos.ntc_temp);
--- power/buckboost.c
+++ power/buckboost.c
@@ -365,6 +365,7 @@ void buckboost_protection_handle(void)
 	}
 #if(CONFIG_USE_NTC_FOR_CHAGER == 1)
 	if(gd->typec_ntc_lock||gd->bat_ntc_lock_flag||bat_ntc_dual_dischg_lock) status|=VBUS_FAULT_VBUS_NTC;
+	printk("VBUS_FAULT_VBUS_NTC\n");
 #endif
 #endif
 	if(gd->led_fault&&!(status&0x6060)&&!bat_ntc_stop_chrg_flag&&!(gd->typec_charge_ntc_lock&&g_buckboost.woke_mode == BUCKBOOST_CHAGER_MODE)&&!gd->vbus_ovp)
